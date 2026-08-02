9d836cee562d4f1e60582da0449abc0a43f454c9
semeh <semeh.missaoui@gmail.com>
2026-08-02 08:51:17 +0100

    Ajout des exercices JavaScript

--- probleme1.js
+++ probleme1.js
@@ -1,5 +1,9 @@
 // 1-Créer une fonction qui combine ton prénom et ton nom et retourne le résultat avec un espace entre les deux.
+function fullName(prenom, nom) {
+  return `${prenom} ${nom}`;
+}
 
+console.log(fullName("Semeh", "Missaoui"));
 
 
 
@@ -9,7 +13,11 @@
 
 
 // 2-Écris une fonction qui prend deux nombres et retourne la moyenne.
-
+function moyenne(nombre1, nombre2) {
+  return (nombre1 + nombre2) / 2;
+}
+ 
+console.log(moyenne(10, 20));
 
 
 
@@ -19,7 +27,11 @@
 
 
 // 3-Crée une fonction qui prend une chaîne de caractères et retourne le nombre de caractères qu'elle contient.
+function nombreCaracteres(chaine) {
+  return chaine.length;
+}
 
+console.log(nombreCaracteres("missaoui semeh"));
 
 
 
@@ -29,6 +41,12 @@
 
 
 // 4-Crée une fonction qui retourne la sous-chaîne à partir d'une position donnée jusqu'à la fin.
+function sousChaine(chaine, position) {
+  return chaine.substring(position);
+}
+
+
+console.log(sousChaine("semeh", 2));
 
 
 
--- probleme2.js
+++ probleme2.js
@@ -1,13 +1,26 @@
 // 1- Crée une fonction qui prend deux nombres et retourne :
 // "égaux" s’ils sont identiques
 // "différents" sinon
-
+function comparerNombres(nombre1, nombre2) {
+  if (nombre1 === nombre2) {
+    return "égaux";
+  } else {
+    return "différents";
+  }
+}
+console.log(comparerNombres(14, 14));
+console.log(comparerNombres(10, 5));  
 
 
 
 // 2- Crée une fonction qui prend une chaîne et retourne true
 // si elle est vide, sinon false
+function estVide(chaine) {
+  return chaine === "";
+}
 
+console.log(estVide(""));        
+console.log(estVide("semeh")); 
 
 
 
@@ -15,7 +28,19 @@
 // "lent" si inférieure à 30
 // "normal" si entre 30 et 90
 // "rapide" si supérieure à 90
+function vitesse(vitesse) {
+  if (vitesse < 30) {
+    return "lent";
+  } else if (vitesse <= 90) {
+    return "normal";
+  } else {
+    return "rapide";
+  }
+}
 
+console.log(vitesse(20));   
+console.log(vitesse(50));   
+console.log(vitesse(120));  
 
 
 
@@ -23,4 +48,14 @@
 // 4- Crée une fonction qui prend un nombre et retourne true
 // s’il est divisible par 2 OU par 3
 // MAIS PAS par les deux en même temps
+function estDivisible(nombre) {
+  const divisiblePar2 = nombre % 2 === 0;
+  const divisiblePar3 = nombre % 3 === 0;
 
+  return divisiblePar2 !== divisiblePar3;
+}
+ 
+console.log(estDivisible(4)); 
+console.log(estDivisible(6));  
+console.log(estDivisible(9));      
+console.log(estDivisible(5));   
