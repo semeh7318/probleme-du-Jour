3d476a153d696571805991600e6dea62992b8a45
semeh <semeh.missaoui@gmail.com>
2026-09-13 08:33:43 +0100

    problem

--- probleme13.js
+++ probleme13.js
@@ -6,8 +6,23 @@
  * moveZeros([0, 4, 0, 7, 2, 0, 5])  // [4, 7, 2, 5, 0, 0, 0]
  */
 var moveZeros = function(arr) {
-  // Votre code ici.
+  var resultat = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      resultat.push(arr[i]);
+    }
+  }
+
+  var nombreDeZeros = arr.length - resultat.length;
+
+  for (var j = 0; j < nombreDeZeros; j++) {
+    resultat.push(0);
+  }
+
+  return resultat;
 };
+
 
 /**
  * Un tableau contient les nombres de 1 à n avec un seul nombre manquant.
@@ -16,5 +31,22 @@ var moveZeros = function(arr) {
  * missingNumber([1, 2, 4, 5, 6])  // 3
  */
 var missingNumber = function(arr) {
-  // Votre code ici.
+
+  var n = arr.length + 1;
+  var sommeAttendue = (n * (n + 1)) / 2;
+
+  var sommeReelle = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sommeReelle += arr[i];
+  }
+
+  return sommeAttendue - sommeReelle;
 };
+//try
+console.log(moveZeros([0, 4, 0, 7, 2, 0, 5])); 
+console.log(moveZeros([1, 0, 0, 3, 0, 5]));    
+console.log(moveZeros([0, 0, 0]));             
+
+console.log(missingNumber([1, 2, 4, 5, 6])); 
+console.log(missingNumber([2, 3, 4, 5]));    
+console.log(missingNumber([1, 2, 3, 5]));    
